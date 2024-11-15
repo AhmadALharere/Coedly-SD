531b2cca191142c25142bd498165e79223b5570e
AhmadALharere <ite.ahmadalharere@gmail.com>
2024-11-15 14:18:13 +0300

    fix some pug

--- channel/urls.py
+++ channel/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:id>/ins',views.Show_channel_insights,name='Channelinsights'),
     path('<int:id>/follow',views.follow,name='followChannel'),
     path('<int:id>/unfollow',views.unfollow,name='UnfollowChannel'),
-    path('<int:id>',views.Show_channel_Detail,name='ChannelDetails'),
+    path('<int:id>',views.Show_channel_Detail.as_view(),name='ChannelDetails'),
     ]
 
 
